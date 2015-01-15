f79cd76bc9564a4a12115811fc85cb6fad917b08
Jozef Izso <jozef.izso@gmail.com>
2015-01-15 22:58:07 +0100

    Moved protocol classes to their own files.

---
+++ AsyncPluggableProtocol/IProtocol.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncPluggableProtocol
+{
+    public interface IProtocol
+    {
+        string Name { get; }
+
+        Task<Stream> GetStreamAsync(string url);
+    }
+}
