e31f07a334085588c9dd9f67ca22210b2e60df2c
Jozef Izso <jozef.izso@gmail.com>
2015-01-15 22:35:22 +0100

    Removed old code.

--- AsyncPluggableProtocol/Properties/AssemblyInfo.cs
+++ AsyncPluggableProtocol/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Information about an assembly.
 [assembly: AssemblyTitle("AsyncPluggableProtocol")]
 [assembly: AssemblyDescription("Handle custom protocol schemes when you are using the WebBrowser control.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Configure COM visibility and typelib identifier.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b459eb1d-5333-40a5-92d9-97e96059864c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Version information for assembly.
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
